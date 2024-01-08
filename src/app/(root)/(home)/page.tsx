cd6779a409a9d99f78411d12e12c1f246fb0f4ac
MJ Habib <mj.habib4@gmail.com>
2024-01-08 15:16:01 +0330

    finish part06-07, Architecture & Authentication by Clerk

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,6 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function HomePage() {
   return (
     <div>
+      <UserButton afterSignOutUrl='/' />
       <h1 className='h1-bold'>Home Page</h1>
     </div>
   );
